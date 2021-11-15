7b904a97a8a940b99b90586b7ad70cbdd7c17483
galip16 <galipsvt16@gmail.com>
2021-11-15 14:53:03 +0100

    about spelling mistakes corrected

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,28 +16,28 @@ function About() {
           </Card.Header>
           <Card.Text className=" cardText text-justify border">
             <p>
-              2000’li yıllarından başında “Krokan Helva” üretmeye başlayarak
-              girdiğimiz sektörde ürün yelpazemize “Osmanlı Macununu” katarak
-              Bursa ili çevresinde gerçekleşen Ertuğrul Gazi’yi anma ve Söğüt
-              Şenlikleri ve “İznik Panayırı” başta olmak üzere birçok etkinlikte
+              2000'li yıllarından başında "Krokan Helva" üretmeye başlayarak
+              girdiğimiz sektörde ürün yelpazemize "Osmanlı Macununu" katarak
+              Bursa ili çevresinde gerçekleşen Ertuğrul Gazi'yi anma ve Söğüt
+              Şenlikleri ve "İznik Panayırı" başta olmak üzere birçok etkinlikte
               yer aldık.
             </p>
 
             <p>
               2005 yili itibari ile klasik Pamuk Şeker anlayışını yıkarak kendi
-              ürettiğimiz makineler ile “Pamuk Şeker” satışına başladık. Evlilik
+              ürettiğimiz makineler ile "Pamuk Şeker" satışına başladık. Evlilik
               ve sünnet düğünlerinde misafirlerine ürünlerimizi ikram etmek
               isteyen müşterilerimiz bizlere ulaşmaya başladı.Müşteri
               memnuniyeti artınca kendimizi geliştirmeye yönelik Ar-ge
-              çalışmaları yürüttük ve kadromuza “Patlamış Mısır”, “Salep”, “Elma
-              Şekeri” kattık. Artık Bursa çapında düzenlenen birçok festivalde
-              görev almaya başladık.{" "}
+              çalışmaları yürüttük ve kadromuza "Patlamış Mısır", "Salep", "Elma
+              Şekeri" kattık. Artık Bursa çapında düzenlenen birçok festivalde
+              görev almaya başladık.
             </p>
 
             <p>
               {" "}
-              2010 yılında Osmanlı Mutfağının şifalı içeceklerinden “Osmanlı
-              Şerbetini” orijinal tarifiyle ürettik ve yazın sıcağında
+              2010 yılında Osmanlı Mutfağının şifalı içeceklerinden "Osmanlı
+              Şerbetini" orijinal tarifiyle ürettik ve yazın sıcağında
               müşterilerimizin ferahlamasına vesile olduk. Nostalji Şekerleme
               olarak amacımız insanlara sağlıklı, olabildiğince doğal ve
               kaliteli yeni ürünler sunabilmek ve organizasyonlara tat
